87ac33c41b3923f0cf01bef1327a0c753721d52f
Rakib Rahman <rahmanrakib24@gmail.com>
2025-10-19 10:58:38 -0400

    Assert added

--- ButtonConcept/src/test/java/Button.java
+++ ButtonConcept/src/test/java/Button.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -27,8 +28,10 @@ public class Button {
             WebElement cart= cartButtons.get(index);
             cart.click();
             //System.out.println(cart.getText());
-           String text=  wait.until(ExpectedConditions.elementToBeClickable(cart)).getText();
-            System.out.println(text);
+            String actual="Follow";
+           String expected=  wait.until(ExpectedConditions.elementToBeClickable(cart)).getText();
+            System.out.println(expected);
+            Assert.assertTrue(true);
         }
 
 
